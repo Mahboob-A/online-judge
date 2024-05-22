c8d26598ea053e5e204ed58c53e97d01e3f791d1
Mahboob-A <iammahboob.a@gmail.com>
2024-05-22 11:53:49 +0530

    add: API for testing the online judge

---
+++ src/core_apps/judge_engine/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+
+from django.urls import path
+from core_apps.judge_engine.views import CodeSubmitSimpleImplementation, CodeSubmitRobustAPI
+
+urlpatterns = [
+    path(
+        "simple-execute/",
+        CodeSubmitSimpleImplementation.as_view(),
+        name="simple_execute_code",
+    ),
+    path("robust-execute/", CodeSubmitRobustAPI.as_view(), name="robust_execute_code"),
+]
